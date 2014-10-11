cf4b7be099a8db5b3beea32151247b3cb6a3b1a6
Avram88 <avramscp@gmail.com>
2014-10-11 17:40:29 +0200

    JSTree icons
    Setting icon from JSTree. Root have a folder icon, children file icon.

--- js/index.js
+++ js/index.js
@@ -6,47 +6,57 @@ $(document).ready(function() {
 	$('body').layout({ applyDemoStyles: true });
 		
 	$("#model").jstree({
-	"core" : {
-	  "check_callback" : true,
-	  'data' : [{ "id" : "j1_0", "parent" : "#", "text" : "Entity" }]
-	},
-	"plugins" : [ "dnd" ]
+		"core" : {
+			"check_callback" : function (operation) {
+				return operation === "move_node" ? false : true;
+			},
+			'data' : [{ "id" : "j1_0", "parent" : "#", "text" : "Entity", "icon" : "jstree-folder" }]
+		},	
+		'types' : {
+			"default" : {
+				"icon" : "jstree-file"
+			}			
+		},
+		"plugins" : [ 'dnd', 'types' ]
 	});
 	
 	$('#view').jstree({
-	'core' : {
-		"check_callback" : true,
-		'data' : [{ "id" : "j1_0", "parent" : "#", "text" : "Entity" }]
-	}
+		'core' : {
+			"check_callback" : true,
+			'data' : [{ "id" : "j1_0", "parent" : "#", "text" : "Entity", "icon" : "jstree-folder" }]
+		},	
+		'types' : {
+			"default" : {
+				"icon" : "jstree-file"
+			}	
+		},
+		"plugins" : ['types' ]
 	});
 	
 	$('#model').on('loaded.jstree', function(){
 		loadNodes('');		
 	});
 	
-	$(document)
-		.on('dnd_move.vakata', function(e, data){
-			var t = $(data.event.target);
-			if(t.closest('.drop').length) {
-				data.helper.find('.jstree-icon').removeClass('jstree-er').addClass('jstree-ok');
-			}
-			else{
-				data.helper.find('.jstree-icon').removeClass('jstree-ok').addClass('jstree-er');
-			}
-		}).on('dnd_stop.vakata', function(e, data){
-			var t = $(data.event.target);
-			if(t.closest('.drop').length) {
-				var $view = $('#view');
-				var $root = $view.jstree(true).get_node('j1_0');
-				$view.jstree(true).create_node($root, data.element.text);
-				$view.jstree(true).open_all();
-			}
-		});
+	$(document).on('dnd_move.vakata', function(e, data){
+		var t = $(data.event.target);			
+		if(t.closest('.drop').length) 
+			data.helper.find('.jstree-icon').removeClass('jstree-er').addClass('jstree-ok');		
+		else
+			data.helper.find('.jstree-icon').removeClass('jstree-ok').addClass('jstree-er');		
+	}).on('dnd_stop.vakata', function(e, data){
+		var t = $(data.event.target);
+		
+		if(t.closest('.drop').length) {
+			var $view = $('#view');
+			var $root = $view.jstree(true).get_node('j1_0');
+			$view.jstree(true).create_node($root, data.element.text);
+			$view.jstree(true).open_all();
+		}
+	});
 	
 	$('#search').keyup(function(e){
-		if(e.which == 13){			//13 is enter in all browsers
+		if(e.which == 13)		//13 is enter in all browsers
 			loadNodes($(this).val());
-		}
 	})
 	
 	function loadNodes(str){
